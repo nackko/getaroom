e8dce5c0c8ea780b6405cda9d288cfefe25fb3bf
F8Full <f8full@gmail.com>
2018-05-05 18:31:12 -0400

    #getaroom  —  Android Things&Google Calendar API.
    -initial commit
        -output of Android Studio new project wizard with 2 modules (one mobile, one things) targeting API level 27
        -minor fixes for Kotlin (jdk instead of jre) and explicit Android Things version

---
+++ mobile/src/main/java/com/ludoscity/getaroom/MobileMainActivity.kt
@@ -0,0 +1,12 @@
+package com.ludoscity.getaroom
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MobileMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobile_main)
+    }
+}
